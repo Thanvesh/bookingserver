53e26b73ac6e85f718ae93f8fc9ad5fa58f28dc0
Thanvesh <nanicherry2312@gmail.com>
2024-09-02 13:01:30 +0530

    new3

--- server.js
+++ server.js
@@ -16,12 +16,7 @@ connectDB();
 const app = express();
 
 
-app.use(cors({
-  origin: 'https://radhahotelbooking.netlify.app', // Replace with your frontend's URL
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-  credentials: true,
-}));
+app.use(cors())
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
